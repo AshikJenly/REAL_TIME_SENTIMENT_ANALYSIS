491611e947f82631b178be801569fe8bdd148b52
AshikJenly <jenlyashik@gmail.com>
2023-09-24 17:58:51 +0530

    Added Live Stream Storing

--- 01-StoringDataIntoHive/DataSource.py
+++ 01-StoringDataIntoHive/DataSource.py
@@ -1,5 +1,8 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import split, col
+from pyspark.sql.functions import split, col,udf,concat_ws
+from textblob import TextBlob
+from pyspark.sql.types import StringType
+
 
 spark = SparkSession.builder \
     .appName("SparkStreamingToHive") \
@@ -8,8 +11,7 @@ spark = SparkSession.builder \
 
 kafka_bootstrap_servers = "localhost:9092" 
 kafka_topics = "orders"
-base_storage_path = "/home/projects/SentimentAnalysis"
-hive_table = "STMTDATA.tweet_data"
+hive_table = "STMTDATA.datatweet"
 
 kafka_source = spark.readStream \
     .format("kafka") \
@@ -18,13 +20,28 @@ kafka_source = spark.readStream \
     .option("subscribe", "orders") \
     .load()
 
+
+def getSentiment(text):
+    analysis = TextBlob(text)
+    return "Positive" if analysis.sentiment.polarity > 0 else "Negative" if analysis.sentiment.polarity < 0 else "Neutral"
+sentiment_udf = udf(getSentiment, StringType())
+
+
 # Split the Kafka message values and select the columns
 split_data = kafka_source.select(split(col("value"), ",").alias("split_data"))
 kafka_df_with_columns = split_data.select(
     col("split_data")[0].alias("tweet_id"),
     col("split_data")[1].alias("username"),
-    col("split_data")[2].alias("tweet_text")
+    "split_data"
 )
+
+def concatenate_elements(arr):
+    return ",".join(arr[2:-1])
+concat_udf = udf(concatenate_elements, StringType())
+
+kafka_df_with_columns = kafka_df_with_columns.withColumn("tweet_text", concat_udf(kafka_df_with_columns["split_data"]))
+kafka_df_with_columns = kafka_df_with_columns.drop("split_data")
+kafka_df_with_columns = kafka_df_with_columns.withColumn("sentiment",sentiment_udf(kafka_df_with_columns["tweet_text"]))
 
 def write_to_hive(df, epoch_id):
     df.write \
